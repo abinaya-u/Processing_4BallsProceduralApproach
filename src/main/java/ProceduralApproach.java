d175aa89964d6d1442feb8337b1b75304845b0dc
Abinaya U <abinaya.u@thoughtworks.com>
2021-04-29 20:40:00 +0530

    Add. Created a ball moving from left to right

--- src/main/java/ProceduralApproach.java
+++ src/main/java/ProceduralApproach.java
@@ -1,13 +1,24 @@
 import processing.core.PApplet;
 
 public class ProceduralApproach extends PApplet{
+    private final int HEIGHT = 640;
+    private final int WIDTH = 640;
+    private int x = 0;
+    private int y = HEIGHT/5;
+    private final int DIAMETER = 25;
+
     public static void main(String[] args) {
         PApplet.main("ProceduralApproach",args);
     }
     @Override
     public void settings() {
         super.settings();
-        size(640,640);
+        size(WIDTH,HEIGHT);
     }
 
+    @Override
+    public void draw() {
+        ellipse(x,y,DIAMETER,DIAMETER);
+        x++;
+    }
 }
