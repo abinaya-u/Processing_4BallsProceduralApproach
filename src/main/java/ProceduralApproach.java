22c0ff05eff45478a54d907dd8be23d0c10fc0a9
Abinaya U <abinaya.u@thoughtworks.com>
2021-04-29 20:34:14 +0530

    Add. Defined width and height for the window

--- src/main/java/ProceduralApproach.java
+++ src/main/java/ProceduralApproach.java
@@ -1,7 +1,13 @@
 import processing.core.PApplet;
 
-public class ProceduralApproach {
+public class ProceduralApproach extends PApplet{
     public static void main(String[] args) {
         PApplet.main("ProceduralApproach",args);
     }
+    @Override
+    public void settings() {
+        super.settings();
+        size(640,640);
+    }
+
 }
